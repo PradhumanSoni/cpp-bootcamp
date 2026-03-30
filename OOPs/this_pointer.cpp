01e3bebc1b9c3827364a55d8f7fada55acbe6ae9
pradhumansoni2004 <pradhumansoni2004@gmail.com>
2026-03-30 11:19:53 +0530

    commits

--- OOPs/this_pointer.cpp
+++ OOPs/this_pointer.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 using namespace std;
-class Bank{
-    string name;
-    int account_number;
-    int balance;
+class Distance{
+    int metre, cm;
     public:
-    Bank(string name, int account_number, int balance){
-        name = name;
-        account_number = account_number;
-        balance = balance;
+    Distance(int metre = 0, int cm = 0){
+        this->metre = metre;
+        this->cm = cm;
+    }
+    Distance add(Distance d){
+        Distance temp;
+        temp.cm = cm + d.cm;
+        temp.metre = metre + d.metre + temp.cm/100;
+        temp.cm = temp.cm%100;
+        return temp;
+    }
+    void display(){
+        cout<<metre<<"m "<<cm<<"cm"<<endl;
     }
 };
 
 int main(){
-    Bank b("rohit",123,567);
-    return 0;
+    Distance d1(5,67),d2(34,68),d3;
+    d3 = d1.add(d2);
+    d1.display();
+    d2.display();
+    cout<<endl;
+    d3.display();
+
 }
